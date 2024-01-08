522faa4923ff735ea030a7ee9410ffc7db8cda51
1arin <aroundthegreat@gmail.com>
2024-01-09 01:07:57 +0700

    add DialogContent, show_date_picker, on_save Function

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.pickers import MDDatePicker
+from datetime import datetime
+
+class DialogContent(MDBoxLayout):
+    #this is for class constructor
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.date_text.text=datetime.now().strtime("%A %d %B %Y")
+
+    #this func wil show the date picker
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save = self.on_save)
+
+    #this func will get date and save in friendly form
+    def on_save(self, instance, value, date_range):
+        date = value.strftime("%A %d %B %Y")
+        self.ids.date_text.text = str(date)
+
+    
+
+
+
+
+
+
+
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette=("Teal")
 
-app = MainApp()
-app.run()
+
+
+
+if __name__ == "__main__":
+    MainApp().run()
     
