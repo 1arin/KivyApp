9dd58cb976ddcfc9165d2a0a51078ffcb61c6d52
1arin <aroundthegreat@gmail.com>
2024-01-09 02:04:24 +0700

    add function

--- main.py
+++ main.py
@@ -20,21 +20,28 @@ class DialogContent(MDBoxLayout):
         date = value.strftime("%A %d %B %Y")
         self.ids.date_text.text = str(date)
 
-    
-
-
-
-
-
-
-
-
 
 class MainApp(MDApp):
+    task_list_dialog = None
     def build(self):
         self.theme_cls.primary_palette=("Teal")
 
+    def show_task_function(self):
+        if not self.task_list_dialog:
+            self.task_list_dialog = MDDialog(
+                title = "Create Task",
+                type="custom",
+                content_cls = DialogContent()
+            )
+            self.task_list_dialog.open()
 
+    def add_task(self, task, task_date):
+        print(task.text, task_date)
+
+    
+    
+    def close_dialog(self, **kwargs):
+        self.task_list_dialog.dismiss()
 
 
 if __name__ == "__main__":
