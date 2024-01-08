0c5de3c580bbda304d75a2aa08f9a4d174a4f8b0
1arin <aroundthegreat@gmail.com>
2024-01-08 19:24:17 +0700

    fix error

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from kivymd.app import MDApp
 
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette("Teal")
+        self.theme_cls.primary_palette=("Teal")
 
-
-if __name__ == "__name__":
-    MainApp().run()
+app = MainApp()
+app.run()
     
