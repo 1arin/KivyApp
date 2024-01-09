c653517678afec999ffa66791238146948b72f27
1arin <aroundthegreat@gmail.com>
2024-01-09 23:42:13 +0700

    create a class call ListItemWithCheckbox

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.pickers import MDDatePicker
+
+from kivymd.uix.list import TwoLineAvatarIconListItem , ILeftBody
+from kivymd.uix.selectioncontrol import MDCheckbox
+
 from datetime import datetime
+
 
 class DialogContent(MDBoxLayout):
     #this is for class constructor
@@ -14,11 +19,30 @@ class DialogContent(MDBoxLayout):
     def show_date_picker(self):
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save = self.on_save)
+        date_dialog.open()
 
     #this func will get date and save in friendly form
     def on_save(self, instance, value, date_range):
         date = value.strftime("%A %d %B %Y")
         self.ids.date_text.text = str(date)
+
+#Class for marking and Deleting list
+class ListItemWithCheckbox(TwoLineAvatarIconListItem):
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        self.pk = pk
+
+    #Marking item as complete or incomplete
+    def mark(self, check, the_list_item):
+        if check.active == True:
+            the_list_item.text = '[s]' + the_list_item.text + '[s]'
+        else:
+            pass
+
+    #deleting list item
+    def delete_item(self, the_list_item):
+        self.parent.remove_widget(the_list_item)
+
 
 
 class MainApp(MDApp):
