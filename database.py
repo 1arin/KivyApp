184e436e1fe90301a1e2b30053d2446c23c44a97
1arin <aroundthegreat@gmail.com>
2024-01-11 01:42:32 +0700

    create class database

--- database.py
+++ database.py
@@ -3,4 +3,8 @@ import sqlite3
 class Database():
     def __init__(self):
         self.con = sqlite3.connect("task-database.db")
-        self.cursor = 
+        self.cursor = self.con.corsor()
+
+    def create_task_table(self):
+        self.corsor.execute("CREATE TABLE IF NOT EXISTS task(id integer PRIMARY KEY AUTOINCREMENT , YASK VARCHAR(50) NOT NULL, complete BOOLEAN NOT NULL CHECK (completedIN (0,1)))")
+        self.con.commit()
